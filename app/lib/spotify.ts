1e36a49164648d036aaadc100b18bdfc4c647aa7
marcelscr <marcel.reboucas@inlocomedia.com>
2021-12-09 18:17:55 -0300

    refactor(spotify): rewrite the spotify api helper

--- app/lib/spotify.ts
+++ app/lib/spotify.ts
@@ -1,56 +1,53 @@
 import SpotifyWebApi from 'spotify-web-api-node'
 import { commitSession, getSession } from '~/services/session.server'
 import { AuthTokens } from '~/types'
+import { Session } from 'remix'
+
+async function refreshAccessToken(api: SpotifyWebApi, session: Session) {
+  try {
+    const response = await api.refreshAccessToken()
+
+    const accessToken = response.body.access_token
+    const refreshToken = response.body.refresh_token
+    const expiresAt = Date.now() + (response.body.expires_in - 120) * 1000 // Refresh two minutes earlier
+
+    // Update the API
+    api.setAccessToken(accessToken)
+    api.setRefreshToken(refreshToken ?? '')
+
+    // Update the Session
+    session.set('accessToken', accessToken)
+    session.set('refreshToken', refreshToken)
+    session.set('expiresAt', expiresAt)
+    await commitSession(session)
+  } catch (error) {
+    console.error(error)
+    throw error
+  }
+}
+
+function hasValidAccessToken(session: Session) {
+  return (
+    session.get('accessToken') &&
+    session.get('refreshToken') &&
+    session.get('expiresAt') &&
+    Date.now() < session.get('expiresAt')
+  )
+}
 
 async function spotifyApi(request: Request, tokens: AuthTokens) {
   const spotifyApi = new SpotifyWebApi({
     clientId: process.env.SPOTIFY_CLIENT_ID,
-    clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    accessToken: tokens.accessToken,
+    refreshToken: tokens.refreshToken
   })
-  spotifyApi.setAccessToken(tokens.accessToken)
-  spotifyApi.setRefreshToken(tokens.refreshToken)
-
-  async function refreshAccessToken() {
-    try {
-      const response = await spotifyApi.refreshAccessToken()
-      return {
-        accessToken: response.body.access_token,
-        refreshToken: response.body.refresh_token,
-        expiresAt: Date.now() + (response.body.expires_in - 120) * 1000 // Refresh two minutes earlier
-      }
-    } catch (error) {
-      console.error(error)
-      return {
-        error: 'RefreshAccessTokenError'
-      }
-    }
-  }
 
   const session = await getSession(request)
 
-  const hasValidAccessToken =
-    session.get('accessToken') &&
-    session.get('refreshToken') &&
-    session.get('expiresAt') &&
-    Date.now() >= session.get('expiresAt')
-
-  console.log({ now: Date.now(), expires: session.get('expiresAt') })
-
-  if (!hasValidAccessToken) {
-    const { accessToken, refreshToken, expiresAt, error } =
-      await refreshAccessToken()
-
-    if (error) throw new Error(error)
-
-    session.set('accessToken', accessToken)
-    session.set('refreshToken', refreshToken)
-    session.set('expiresAt', expiresAt)
-
-    await commitSession(session)
+  if (!hasValidAccessToken(session)) {
+    await refreshAccessToken(spotifyApi, session)
   }
-
-  spotifyApi.setAccessToken(session.get('accessToken'))
-  spotifyApi.setRefreshToken(session.get('refreshToken'))
 
   return spotifyApi
 }
