67753674b8faeaf11f7b8f5f5e0ebdef2a7cf812
marcelscr <marcel.reboucas@inlocomedia.com>
2021-12-13 21:51:36 -0300

    feat(header): change the header colors

--- app/components/LayoutHeader.tsx
+++ app/components/LayoutHeader.tsx
@@ -10,7 +10,7 @@ function LayoutHeader() {
   return (
     <div className="relative">
       <header className="absolute top-5 right-8">
-        <div className="flex items-center bg-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 p-r-2">
+        <div className="flex items-center bg-black text-white space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 p-r-2">
           <img
             className="rounded-full w-10 h-10"
             src={user.image}
